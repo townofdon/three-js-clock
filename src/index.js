9245ba0786a6a2769160ab1e0209de24cba5abd1
Don Townsend <don@teamairship.com>
2020-10-15 02:37:39 -0400

    add 3d clock

--- src/index.js
+++ src/index.js
@@ -6,6 +6,114 @@ const DEBUG = false;
 
 let camera, scene, renderer, cube, spotLight, controls, lightHelper, shadowCameraHelper;
 
+const clock = {};
+const clockMaterial = new THREE.MeshPhongMaterial({ color: 0xb0a780, dithering: true });
+const clockYOffset = 1;
+
+function setClockBackface() {
+  const radiusTop = 4;  
+  const radiusBottom = 4;  
+  const height = 0.1;
+  const radialSegments = 50;  
+  const geometry = new THREE.CylinderBufferGeometry(radiusTop, radiusBottom, height, radialSegments);
+  const mesh = new THREE.Mesh(geometry, clockMaterial);
+  // rotate towards the camera
+  mesh.rotation.x += Math.PI * 0.5;
+  mesh.position.y += clockYOffset;
+  clock.backface = mesh;
+}
+
+function setClockFrame() {
+  const arcShape = new THREE.Shape()
+    .moveTo( 5, 0 )
+    .absarc( 0, 0, 4, 0, Math.PI * 2, false );
+  const holePath = new THREE.Path()
+    .moveTo( 4.65, 0 )
+    .absarc( 0, 0, 3.65, 0, Math.PI * 2, true );
+  arcShape.holes.push( holePath );
+  const arcShapePrecise = new THREE.Shape(arcShape.extractPoints(50).shape);
+  arcShapePrecise.holes.push(new THREE.Path(arcShape.getPointsHoles(50)[0]));
+  const extrudeSettings = {
+    depth: 0.2,
+    bevelEnabled: false,
+    steps: 5,
+  };
+  const geometry =  new THREE.ExtrudeGeometry(arcShapePrecise, extrudeSettings);
+  const material = new THREE.MeshPhongMaterial({ color: 0x050505, dithering: true });
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.y += clockYOffset;
+  clock.frame = mesh;
+}
+
+function setClockHourHand() {
+  const shape = new THREE.Shape([
+    new THREE.Vector2( 0, 0.20 ),
+    new THREE.Vector2( 0.25, 0.15 ),
+    new THREE.Vector2( 0.25, -0.15 ),
+    new THREE.Vector2( 0, -0.20 ),
+    new THREE.Vector2( -2, -0.05 ),
+    new THREE.Vector2( -2, 0.05 ),
+  ]);
+  const extrudeSettings = {
+    depth: 0.1,
+    bevelEnabled: false,
+    steps: 5,
+  };
+  const geometry =  new THREE.ExtrudeGeometry(shape, extrudeSettings);
+  const material = new THREE.MeshPhongMaterial({ color: 0x353025, dithering: true });
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.z += 0.2;
+  mesh.position.y += clockYOffset;
+  mesh.rotation.z -= Math.PI * 0.5;
+  clock.hourHand = mesh;
+}
+
+function setClockMinuteHand() {
+  const shape = new THREE.Shape([
+    new THREE.Vector2( 0, 0.15 ),
+    new THREE.Vector2( 0.25, 0.10 ),
+    new THREE.Vector2( 0.25, -0.10 ),
+    new THREE.Vector2( 0, -0.15 ),
+    new THREE.Vector2( -3, -0.05 ),
+    new THREE.Vector2( -3, 0.05 ),
+  ]);
+  const extrudeSettings = {
+    depth: 0.1,
+    bevelEnabled: false,
+    steps: 5,
+  };
+  const geometry =  new THREE.ExtrudeGeometry(shape, extrudeSettings);
+  const material = new THREE.MeshPhongMaterial({ color: 0x000000, dithering: true });
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.z += 0.25;
+  mesh.position.y += clockYOffset;
+  mesh.rotation.z -= Math.PI * 0.75;
+  clock.minuteHand = mesh;
+}
+
+function setClockSecondHand() {
+  const shape = new THREE.Shape([
+    new THREE.Vector2( 0, 0.05 ),
+    new THREE.Vector2( 0.75, 0.03 ),
+    new THREE.Vector2( 0.75, -0.03 ),
+    new THREE.Vector2( 0, -0.05 ),
+    new THREE.Vector2( -3, -0.01 ),
+    new THREE.Vector2( -3, 0.01 ),
+  ]);
+  const extrudeSettings = {
+    depth: 0.05,
+    bevelEnabled: false,
+    steps: 5,
+  };
+  const geometry =  new THREE.ExtrudeGeometry(shape, extrudeSettings);
+  const material = new THREE.MeshPhongMaterial({ color: 0xee0100, dithering: true });
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.z += 0.35;
+  mesh.position.y += clockYOffset;
+  mesh.rotation.z -= Math.PI * 1;
+  clock.secondHand = mesh;
+}
+
 function init() {
 	scene = new THREE.Scene();
 
@@ -18,6 +126,7 @@ function init() {
 		1000
 	);
 	camera.position.z = 5;
+	camera.position.y = -3;
 
   // renderer
 
@@ -34,16 +143,6 @@ function init() {
 	// Create material with texture
 	// const material = new THREE.MeshBasicMaterial({ map: texture });
 
-  // cube
-
-	const geometry = new THREE.BoxGeometry(3, 3, 3);
-	const material = new THREE.MeshPhongMaterial({ color: 0x0055ff, dithering: true });
-	cube = new THREE.Mesh(geometry, material);
-  cube.receiveShadow = true;
-  cube.castShadow = true;
-	scene.add(cube);
-  cube.rotation.x += Math.PI * 0.25;
-
   // ground
 
   const groundData = {
@@ -55,6 +154,19 @@ function init() {
   ground.rotation.x = - Math.PI * 0.5;
   ground.receiveShadow = true;
   scene.add( ground );
+
+  // clock
+
+  setClockBackface();
+  setClockFrame();
+  setClockHourHand();
+  setClockMinuteHand();
+  setClockSecondHand();
+  scene.add( clock.backface );
+  scene.add( clock.frame );
+  scene.add( clock.hourHand );
+  scene.add( clock.minuteHand );
+  scene.add( clock.secondHand );
 
   // ambient light
 
@@ -113,7 +225,7 @@ function render() {
 	requestAnimationFrame(render);
 
 	// Rotate cube (Change values to change speed)
-	cube.rotation.y += 0.01;
+	// cube.rotation.y += 0.01;
 
   if (DEBUG) {
     lightHelper.update();

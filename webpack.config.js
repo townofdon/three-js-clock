b18b86a59e1da86bcb061b0f066f5589d77b1c0e
dtownsend <dtownsend@axon.com>
2023-04-06 15:18:44 -0400

    add meta tags; build

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+const meta = {
+  title: "Demo - Simple Three.js Clock",
+  desc: "This is just a demo showcasing a simple 3D clock made using Three.js.",
+  image: "https://townofdon.github.io/three-js-clock/demo.gif",
+  url: "https://townofdon.github.io/three-js-clock/",
+};
+
 module.exports = {
   mode: "development",
   entry: {
@@ -44,6 +51,27 @@ module.exports = {
     new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
     new HtmlWebpackPlugin({
       title: "Three.js Clock",
+      meta: {
+        description: { name: "description", content: meta.desc },
+        "og:title": {
+          property: "og:title",
+          content: meta.title,
+        },
+        "og:description": { property: "og:description", content: meta.desc },
+        "og:type": { property: "og:type", content: "website" },
+        "og:url": { property: "og:url", content: meta.url },
+        "og:image": { property: "og:image", content: meta.image },
+        "twitter:card": {
+          name: "twitter:card",
+          content: "summary_large_image",
+        },
+        "twitter:title": { name: "twitter:title", content: meta.title },
+        "twitter:description": {
+          name: "twitter:description",
+          content: meta.desc,
+        },
+        "twitter:image": { name: "twitter:image", content: meta.image },
+      },
     }),
   ],
   output: {
